d083b8946f3c68d78395511d1f1a1a073d6ad577
Rex Shi <rexshi@pgyer.com>
2022-08-21 00:24:07 +0800

    remove jq and app_type

--- shell-demo/demo.sh
+++ shell-demo/demo.sh
@@ -2,23 +2,19 @@
 #
 # 通过shell脚本来实现将本地app文件通过API上传到蒲公英
 # https://www.pgyer.com/doc/view/api#fastUploadApp
-#
-# 本程序依赖: jq (https://stedolan.github.io/jq/)
-#
+##
 # 参数说明：
 # $1: 蒲公英api_key
-# $2: 要上传的文件路径
-# $3: 安装包文件类型
+# $2: 要上传的文件路径(ipa/apk)
 #
 
 # get the first argument
 readonly api_key=$1
 readonly file=$2
-readonly app_type=$3
 
 printHelp() {
-    echo "Usage: $0 api_key file app_type"
-    echo "Example: $0 <your_api_key> <your_file_path> <app_type>[ios|android]"
+    echo "Usage: $0 api_key file"
+    echo "Example: $0 <your_api_key> <your_apk_or_ipa_path>"
 }
 
 # check api_key exists
@@ -35,20 +31,13 @@ if [ ! -f "$file" ]; then
     exit 1
 fi
 
-# check app_type if is [ios, android]
-if [ -z "$app_type" ]; then
-    echo "app_type is empty"
+if [[ $file =~ ipa$ ]]; then
+    app_type="ios"
+elif [[ $file =~ apk$ ]]; then
+    app_type="android"
+else
+    echo "file type not support"
     printHelp
-    exit 1
-elif [ "$app_type" != "ios" ] && [ "$app_type" != "android" ]; then
-    echo "app_type is not [ios, android]"
-    printHelp
-    exit 1
-fi
-
-# check if jq exists
-if ! which jq > /dev/null; then
-    echo "jq not found, please install it first"
     exit 1
 fi
 
@@ -78,10 +67,11 @@ execCommand() {
 logTitle "获取上传凭证"
 
 execCommand "curl -s -F '_api_key=${api_key}' -F 'buildType=${app_type}' http://www.pgyer.com/apiv2/app/getCOSToken"
-endpoint=`echo $result | jq -r '.data.endpoint'`
-key=`echo $result | jq -r '.data.params.key'`
-signature=`echo $result | jq -r '.data.params.signature'`
-x_cos_security_token=`echo $result | jq -r '.data.params["x-cos-security-token"]'`
+
+[[ "${result}" =~ \"endpoint\":\"([:_-\./\a-z0-9\.]+)\" ]] && endpoint=`echo ${BASH_REMATCH[1]} | sed 's!\\\/!/!g'`
+[[ "${result}" =~ \"key\":\"([\.a-z0-9]+)\" ]] && key=`echo ${BASH_REMATCH[1]}`
+[[ "${result}" =~ \"signature\":\"([\=\&\_\;A-Za-z0-9\-]+)\" ]] && signature=`echo ${BASH_REMATCH[1]}`
+[[ "${result}" =~ \"x-cos-security-token\":\"([\_A-Za-z0-9\-]+)\" ]] && x_cos_security_token=`echo ${BASH_REMATCH[1]}`
 
 # ---------------------------------------------------------------
 # 上传文件
@@ -103,8 +93,8 @@ logTitle "检查结果"
 
 for i in {1..20}; do
     execCommand "curl -s http://www.pgyer.com/apiv2/app/buildInfo?_api_key=${api_key}\&buildKey=${key}"
-    code=`echo $result | jq -r '.code'`
-    if [ $code == '0' ]; then
+    [[ "${result}" =~ \"code\":([0-9]+) ]] && code=`echo ${BASH_REMATCH[1]}`
+    if [ $code -eq 0 ]; then
         log $result
         break
     else
